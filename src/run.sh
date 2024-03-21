9f3de1bc5f1d179c32e12c00eb67ac8ee9136bfa
Bert Spaan <bert.spaan@gmail.com>
2024-03-21 16:44:28 +0000

    see if pmtiles generation works with more memory

--- src/run.sh
+++ src/run.sh
@@ -7,15 +7,15 @@
 wget "https://annotations.allmaps.org/maps.geojson?key=$ALLMAPS_API_KEY&limit=-1" \
   -O ./data/maps.geojson
 
-# cat ./data/maps.geojson | ./src/flatten-geojson.sh > ./data/maps-flattened.geojson
+cat ./data/maps.geojson | ./src/flatten-geojson.sh > ./data/maps-flattened.geojson
 
-# tippecanoe -f -zg --projection=EPSG:4326 --drop-densest-as-needed \
-#   -y "id" -y "scale" -y "area" \
-#   -y "resourceId" -y "resourceType" -y "resourceWidth" -y "resourceHeight" \
-#   -y "imageServiceDomain" \
-#   -o ./data/maps.pmtiles -l masks ./data/maps-flattened.geojson
+tippecanoe -f -zg --projection=EPSG:4326 --drop-densest-as-needed \
+  -y "id" -y "scale" -y "area" \
+  -y "resourceId" -y "resourceType" -y "resourceWidth" -y "resourceHeight" \
+  -y "imageServiceDomain" \
+  -o ./data/maps.pmtiles -l masks ./data/maps-flattened.geojson
 
-# rm ./data/maps-flattened.geojson
+rm ./data/maps-flattened.geojson
 
 # =============================================================================
 # JSON & NDJSON
