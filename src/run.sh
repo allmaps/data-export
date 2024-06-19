22c5a96b568e2ec01b9b96dc34dcb5ffe1baf307
Bert Spaan <bert.spaan@gmail.com>
2024-06-19 08:43:54 +0200

    also add modified to pmtiles

--- src/run.sh
+++ src/run.sh
@@ -14,7 +14,7 @@ cat ./data/maps.geojson | ./src/flatten-geojson.sh > ./data/maps-flattened.geojs
 tippecanoe -f -z8 --simplify-only-low-zooms --full-detail=24 --visvalingam \
   --drop-densest-as-needed \
   --projection=EPSG:4326 \
-  -y "id" -y "scale" -y "area" \
+  -y "id" -y "scale" -y "area" -y "modified" \
   -y "resourceId" -y "resourceType" -y "resourceWidth" -y "resourceHeight" \
   -y "imageServiceDomain" \
   -o ./data/maps.pmtiles -l masks ./data/maps-flattened.geojson
